1edba1fb829bb778685f7895f4f7b7031db38882
nabuk <niemyjski.jakub@gmail.com>
2013-11-11 01:31:52 +0100

    making use of the global assembly info

---
+++ Src/GlobalAssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyProduct("NSlice")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Jakub Niemyjski 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
