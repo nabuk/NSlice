799bacb6f287005c65a456d5f5bdfd580669bb7b
nabuk <niemyjski.jakub@gmail.com>
2014-01-16 21:07:53 +0100

    enumerable slicedelete +++ case has been implemented

--- Src/NSlice/Helpers/EnumerableSliceDeleteCases.cs
+++ Src/NSlice/Helpers/EnumerableSliceDeleteCases.cs
@@ -9,7 +9,72 @@ namespace NSlice.Helpers
     {
         internal static IEnumerable<T> PPP<T>(IEnumerable<T> source, int from, int? to, int step)
         {
-            throw new NotImplementedException();
+            using (var enumerator = source.GetEnumerator())
+            {
+                for (var i = 0; i < from; ++i)
+                    if (enumerator.MoveNext())
+                        yield return enumerator.Current;
+                    else
+                        yield break;
+
+                if (to.HasValue)
+                {
+                    var toValue = to.Value;
+
+                    if (step == 1)
+                        while (from++ < toValue)
+                        {
+                            if (!enumerator.MoveNext())
+                                yield break;
+                        }
+                    else
+                    {
+                        int reminder;
+                        var iterations = Math.DivRem(toValue - from, step, out reminder);
+
+                        for (var i = 0; i < iterations; ++i)
+                        {
+                            if (!enumerator.MoveNext())
+                                yield break;
+
+                            for (var j = 1; j < step; ++j)
+                                if (enumerator.MoveNext())
+                                    yield return enumerator.Current;
+                                else
+                                    yield break;
+                        }
+
+                        if (reminder > 0)
+                        {
+                            if (!enumerator.MoveNext())
+                                yield break;
+
+                            for (var j = 1; j < reminder; ++j)
+                                if (enumerator.MoveNext())
+                                    yield return enumerator.Current;
+                                else
+                                    yield break;
+                        }
+                    }
+
+                    while (enumerator.MoveNext())
+                            yield return enumerator.Current;
+                }
+                else
+                {
+                    if (step == 1)
+                        yield break;
+
+                    while (enumerator.MoveNext())
+                    {
+                        for (var i = 1; i < step; ++i)
+                            if (enumerator.MoveNext())
+                                yield return enumerator.Current;
+                            else
+                                yield break;
+                    }
+                }
+            }
         }
 
         internal static IEnumerable<T> PPN<T>(IEnumerable<T> source, int from, int? to, int step)
