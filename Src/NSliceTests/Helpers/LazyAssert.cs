9b61303bc72d6668c3fa2ae58f5be5698eafcb91
nabuk <niemyjski.jakub@gmail.com>
2014-01-16 19:16:51 +0100

    LazyAssert - exception messages are formatted on demand

---
+++ Src/NSliceTests/Helpers/LazyAssert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Sdk;
+
+namespace NSliceTests.Helpers
+{
+    public static class LazyAssert
+    {
+        public static void True(bool condition, Func<string> userMessageCallback)
+        {
+            if (!condition)
+                throw new TrueException(userMessageCallback());
+        }
+    }
+}
--- Src/NSliceTests/Tests/EnumerableTests/EnumerableSliceDeleteTests.cs
+++ Src/NSliceTests/Tests/EnumerableTests/EnumerableSliceDeleteTests.cs
@@ -16,13 +16,12 @@ namespace NSliceTests.Tests.EnumerableTests
         public void SliceDelete_FromEnumerableExtensions_ReturnsCorrectValues(int? from, int? to, int step, int length)
         {
             var source = Enumerable.Range(0, length);
-            var sourceArray = source.ToArray();
             var sut = EnumerableExtensions.SliceDelete(source, from, to, step).ToArray();
             var expected = SliceDeleteExpectedResultCalculator.Calculate(from, to, step, length);
 
-            Assert.True(
+            LazyAssert.True(
                 expected.SequenceEqual(sut),
-                ErrorFormatter.FormatSliceDeleteResultError(sourceArray, from, to, step, expected, sut));
+                () => ErrorFormatter.FormatSliceDeleteResultError(source, from, to, step, expected, sut));
         }
 
         [Fact]
@@ -44,9 +43,9 @@ namespace NSliceTests.Tests.EnumerableTests
             var sut = EnumerableExtensions.SliceDelete(source, from, to, step).ToArray();
             var expected = SliceDeleteExpectedResultCalculator.Calculate(from, to, step, length);
 
-            Assert.True(
+            LazyAssert.True(
                 expected.SequenceEqual(sut),
-                ErrorFormatter.FormatSliceDeleteResultError(source, from, to, step, expected, sut));
+                () => ErrorFormatter.FormatSliceDeleteResultError(source, from, to, step, expected, sut));
         }
 
         [Theory, ClassData(typeof(SliceTestCaseSource))]
@@ -56,9 +55,9 @@ namespace NSliceTests.Tests.EnumerableTests
             var sut = EnumerableExtensions.SliceDelete(source, from, to, step).ToArray();
             var expected = SliceDeleteExpectedResultCalculator.Calculate(from, to, step, length);
 
-            Assert.True(
+            LazyAssert.True(
                 expected.SequenceEqual(sut),
-                ErrorFormatter.FormatSliceDeleteResultError(source, from, to, step, expected, sut));
+                () => ErrorFormatter.FormatSliceDeleteResultError(source, from, to, step, expected, sut));
         }
     }
 }
