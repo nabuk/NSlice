85684981cac3b1133daa949806e0952099b6a3b2
nabuk <niemyjski.jakub@gmail.com>
2014-01-16 22:46:55 +0100

    enumerable slicedelete ++- case has been implemented

--- Src/NSlice/Helpers/EnumerableSliceDeleteCases.cs
+++ Src/NSlice/Helpers/EnumerableSliceDeleteCases.cs
@@ -22,11 +22,11 @@ namespace NSlice.Helpers
                     var toValue = to.Value;
 
                     if (step == 1)
+                    {
                         while (from++ < toValue)
-                        {
                             if (!enumerator.MoveNext())
                                 yield break;
-                        }
+                    }
                     else
                     {
                         int reminder;
@@ -58,7 +58,7 @@ namespace NSlice.Helpers
                     }
 
                     while (enumerator.MoveNext())
-                            yield return enumerator.Current;
+                        yield return enumerator.Current;
                 }
                 else
                 {
@@ -79,7 +79,89 @@ namespace NSlice.Helpers
 
         internal static IEnumerable<T> PPN<T>(IEnumerable<T> source, int from, int? to, int step)
         {
-            throw new NotImplementedException();
+            step = -step;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (to.HasValue)
+                {
+                    var toValue = to.Value;
+
+                    for (var i = 0; i <= toValue; ++i)
+                        if (enumerator.MoveNext())
+                            yield return enumerator.Current;
+                        else
+                            yield break;
+
+                    var fromToDiff = from - toValue;
+
+                    if (step == 1)
+                    {
+                        for (int i = 0, b = fromToDiff; i < b; ++i)
+                            if (!enumerator.MoveNext())
+                                yield break;
+                    }
+                    else
+                    {
+                        var buffer = new DynamicBuffer<T>();
+                        buffer.BufferUpToCount(enumerator, fromToDiff);
+
+                        int reminder;
+                        var iterations = Math.DivRem(buffer.length, step, out reminder);
+
+                        var i = 0;
+                        for (var decReminder = reminder - 1; i < decReminder; ++i)
+                            yield return buffer.items[i];
+
+                        if (reminder > 0)
+                            ++i;
+
+                        for (int j = 0, decStep = step - 1; j < iterations; ++j, ++i)
+                            for (var b = i + decStep; i < b; ++i)
+                                yield return buffer.items[i];
+
+                        if (buffer.length < fromToDiff)
+                            yield break;
+                    }
+
+                    while (enumerator.MoveNext())
+                        yield return enumerator.Current;
+                }
+                else
+                {
+                    if (step == 1)
+                    {
+                        while (from-- >= 0)
+                            if (!enumerator.MoveNext())
+                                yield break;
+                    }
+                    else
+                    {
+                        var buffer = new DynamicBuffer<T>();
+                        buffer.BufferUpToCount(enumerator, from + 1);
+
+                        int reminder;
+                        var iterations = Math.DivRem(buffer.length, step, out reminder);
+
+                        var i = 0;
+                        for (var decReminder = reminder - 1; i < decReminder; ++i)
+                            yield return buffer.items[i];
+
+                        if (reminder > 0)
+                            ++i;
+
+                        for (int j = 0, decStep = step - 1; j < iterations; ++j, ++i)
+                            for (var b = i + decStep; i < b; ++i)
+                                yield return buffer.items[i];
+
+                        if (buffer.length < from + 1)
+                            yield break;
+                    }
+
+                    while (enumerator.MoveNext())
+                        yield return enumerator.Current;
+                }
+            }
         }
 
         internal static IEnumerable<T> NNP<T>(IEnumerable<T> source, int from, int to, int step)
