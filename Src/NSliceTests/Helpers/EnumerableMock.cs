fca788170f93775ee45ad9e41e6f105634756d59
nabuk <niemyjski.jakub@gmail.com>
2014-01-23 07:43:57 +0100

    enumerable's mock

---
+++ Src/NSliceTests/Helpers/EnumerableMock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace NSliceTests.Helpers
+{
+    public class EnumerableMock<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> source;
+        private readonly List<EnumeratorMock<T>> enumerators;
+
+        public EnumerableMock(IEnumerable<T> source)
+        {
+            this.source = source;
+            this.enumerators = new List<EnumeratorMock<T>>();
+            this.Enumerators = new ReadOnlyCollection<EnumeratorMock<T>>(this.enumerators);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var enumerator = new EnumeratorMock<T>(this.source.GetEnumerator());
+            this.enumerators.Add(enumerator);
+            this.EnumeratorCreated(enumerator);
+            return enumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IList<EnumeratorMock<T>> Enumerators { get; private set; }
+
+        public event Action<EnumeratorMock<T>> EnumeratorCreated = delegate { };
+    }
+
+    
+}
---
+++ Src/NSliceTests/Helpers/EnumeratorMock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSliceTests.Helpers
+{
+    public class EnumeratorMock<T> : IEnumerator<T>
+    {
+        private readonly IEnumerator<T> source;
+
+        public EnumeratorMock(IEnumerator<T> source)
+        {
+            this.source = source;
+        }
+
+        public int DisposeCallCount { get; private set; }
+
+        public int ResetCallCount { get; private set; }
+
+        #region IEnumerator
+        public T Current
+        {
+            get { return this.source.Current; }
+        }
+
+        public void Dispose()
+        {
+            ++this.DisposeCallCount;
+
+            this.source.Dispose();
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { return this.Current; }
+        }
+
+        public bool MoveNext()
+        {
+            return this.source.MoveNext();
+        }
+
+        public void Reset()
+        {
+            ++this.ResetCallCount;
+
+            this.source.Reset();
+        }
+        #endregion
+    }
+}
